57602a3dfc84691125fe71e429ec5d72f2cb7ff8
Julian <julian@thentia.com>
2016-08-21 18:16:11 -0400

    Initial Commit

---
+++ src/main/resources/web/assets/scripts/sso/com.thentia.sso.app.js
@@ -0,0 +1,11 @@
+console.log ('sso.thentia.com - Your gateway to the future')
+console.log ('Initializing...');
+console.log ('**********************************************************');
+
+var ssoApp = angular.module('com.thentia.sso', ['ngRoute', 'ngAnimate', 'ngSanitize', 'ngCookies', 'ngResource', 'ngMessages', 'kendo.directives']);
+
+ssoApp.filter('unsafe', function ($sce) {
+    return function (val) {
+        return $sce.trustAsHtml(val);
+    };
+});
