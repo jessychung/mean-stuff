44ff77c80bec70cec20fd90e3c801d2ac7e565e4
jessychung <jessyc@thentia.com>
2016-08-22 18:15:35 -0400

    init

--- src/main/resources/web/assets/scripts/sso/controller/com.thentia.sso.controller.default.js
+++ src/main/resources/web/assets/scripts/sso/controller/com.thentia.sso.controller.default.js
@@ -3,4 +3,23 @@ ssoApp.controller('com.thentia.sso.controller.default', function ($http, $routeP
     $scope.session = ssosession;
     $scope.loading = true;
 
+
+    $scope.hideAppBox = function(){
+        $scope.openapps = false;
+    };
+
+});
+
+ssoApp.directive('hideApps', function($document){
+    return {
+        restrict: 'A',
+        link: function(scope, elem, attr, ctrl) {
+            elem.bind('click', function(e) {
+                e.stopPropagation();
+            });
+            $document.bind('click', function() {
+                scope.$apply(attr.hideApps);
+            })
+        }
+    }
 });
