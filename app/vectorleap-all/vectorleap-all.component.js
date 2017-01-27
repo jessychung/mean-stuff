7049b7d7eb540744a099c56a1ba7dda98d479fdc
Jessy Chung <jessyc@thentia.com>
2017-01-27 16:07:22 -0500

    init

--- app/vectorleap-all/vectorleap-all.component.js
+++ app/vectorleap-all/vectorleap-all.component.js
@@ -10,9 +10,11 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 var core_1 = require("@angular/core");
 var vectorleap_service_1 = require("../vectorleap.service");
+var userService_service_1 = require("../userService.service");
 var VectorleapAllComponent = (function () {
-    function VectorleapAllComponent(VectorleapService) {
+    function VectorleapAllComponent(VectorleapService, UserService) {
         this.VectorleapService = VectorleapService;
+        this.UserService = UserService;
         this.VectorleapInstances = [];
     }
     VectorleapAllComponent.prototype.ngOnInit = function () {
@@ -23,6 +25,12 @@ var VectorleapAllComponent = (function () {
             this.VectorleapService.getInstances(currentUserToken)
                 .subscribe(function (res) {
                 _this.VectorleapInstances = res;
+                for (var i = 0; i < _this.VectorleapInstances.length; i++) {
+                    _this.UserService.getUsers(_this.VectorleapInstances[i]._id)
+                        .subscribe(function (res) {
+                        console.log(res);
+                    });
+                }
             });
         }
     };
@@ -32,10 +40,11 @@ VectorleapAllComponent = __decorate([
     core_1.Component({
         selector: 'vectorleap-all',
         templateUrl: 'app/vectorleap-all/vectorleap-all.component.html',
-        providers: [vectorleap_service_1.vectorleapService],
+        providers: [vectorleap_service_1.vectorleapService, userService_service_1.userService],
         encapsulation: core_1.ViewEncapsulation.None
     }),
-    __metadata("design:paramtypes", [vectorleap_service_1.vectorleapService])
+    __metadata("design:paramtypes", [vectorleap_service_1.vectorleapService,
+        userService_service_1.userService])
 ], VectorleapAllComponent);
 exports.VectorleapAllComponent = VectorleapAllComponent;
 //# sourceMappingURL=vectorleap-all.component.js.map
--- app/vectorleap-all/vectorleap-all.component.ts
+++ app/vectorleap-all/vectorleap-all.component.ts
@@ -3,12 +3,13 @@ import { Location }                 from '@angular/common';
 import { Router } from '@angular/router';
 import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 import {vectorleapService} from "../vectorleap.service";
+import {userService} from "../userService.service";
 
 
 @Component ({
     selector: 'vectorleap-all',
     templateUrl: 'app/vectorleap-all/vectorleap-all.component.html',
-    providers: [vectorleapService],
+    providers: [vectorleapService, userService],
     encapsulation: ViewEncapsulation.None
 })
 
@@ -17,7 +18,8 @@ export class VectorleapAllComponent implements OnInit {
     VectorleapInstances = [];
 
     constructor(
-        private VectorleapService: vectorleapService
+        private VectorleapService: vectorleapService,
+        private UserService: userService
     ) {
 
     }
@@ -30,6 +32,12 @@ export class VectorleapAllComponent implements OnInit {
             this.VectorleapService.getInstances(currentUserToken)
                 .subscribe(res => {
                     this.VectorleapInstances = res;
+                    for(var i = 0; i < this.VectorleapInstances.length; i++) {
+                        this.UserService.getUsers(this.VectorleapInstances[i]._id)
+                            .subscribe(res => {
+                                console.log(res);
+                            })
+                    }
                 })
 
         }
